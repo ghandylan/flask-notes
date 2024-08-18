766dc6decead11fbab7ac992493d4bdf92e81d1e
Dylan Louis Tayag <80078209+ghandylan@users.noreply.github.com>
2024-08-18 10:41:12 +0800

    ok

--- static/script/script.js
+++ static/script/script.js
@@ -7,3 +7,21 @@ function logout() {
 function deleteNote() {
     return confirm('Are you sure you want to delete this note?');
 }
+
+function greetTimeOfDay() {
+    // get local of the user
+    const date = new Date();
+    const hour = date.getHours();
+    let greeting = 'Good';
+
+    if (hour >= 0 && hour < 12) {
+        greeting += ' morning';
+    }
+    else if (hour >= 12 && hour < 18) {
+        greeting += ' afternoon';
+    }
+    else {
+        greeting += ' evening';
+    }
+    return greeting;
+}
